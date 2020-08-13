d41d36c4b9be8e9f9de66bd279ec65e867b373da
Taylor Mills <trmills@eagles.usi.edu>
2020-08-13 16:59:10 -0400

    modal

--- src/Components/Dashboard/Calendar/DayContainer.jsx
+++ src/Components/Dashboard/Calendar/DayContainer.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { Button, Modal } from 'semantic-ui-react'
 import Moment from 'moment'
 import classes from '../../../CSS/Dashboard/Calendar.module.css'
 
 const DayContainer = ({ day = '', month, year, nextMonth = false }) => {
   const [showAddButton, setShowAddButton] = useState(false)
+  const [runsModalOpen, setRunsModalOpen] = useState(false)
+
   console.log(day)
   console.log(month)
   console.log(year)
@@ -22,10 +25,19 @@ const DayContainer = ({ day = '', month, year, nextMonth = false }) => {
         <h2 style={{ color: nextMonth ? '#a1a1a1' : 'black', textAlign: 'left' }}>9 mi</h2>
       </div>
       <div style={{ display: 'flex' }}>
-        <button style={{ marginBottom: 8, cursor: 'pointer', visibility: showAddButton ? 'visible' : 'hidden' }}>
-          <Link to={`/activityForm/${day}/${month}/${year}`}>+</Link>
-        </button>
+        <Link to={`/activityForm/${day}/${month}/${year}`}>
+          <Button size='tiny' icon='plus' style={{ marginBottom: 8, visibility: showAddButton ? 'visible' : 'hidden' }} />
+        </Link>
+        <Modal
+          onClose={() => setRunsModalOpen(false)}
+          onOpen={() => setRunsModalOpen(true)}
+          open={runsModalOpen}
+          trigger={<Button size='tiny' icon='write' style={{ marginBottom: 8, visibility: showAddButton ? 'visible' : 'hidden' }} />}
+        >
+          <Modal.Header style={{ textAlign: 'center' }}>Runs for 08/27/2020</Modal.Header>
+        </Modal>
       </div>
+
     </div>
   )
 }
