3c515fd136e05a99427e749065d32b800b3d4d36
Taylor Mills <trmills@eagles.usi.edu>
2020-08-23 17:44:46 -0400

    additions

--- src/Components/ActivityForm/ActivityForm.jsx
+++ src/Components/ActivityForm/ActivityForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useParams, Redirect } from 'react-router-dom'
+import { Button } from 'semantic-ui-react'
 import { createActivity, getActivityByActivityId, updateActivity } from '../../Services/activities'
 import classes from '../../CSS/ActivityForm/ActivityForm.module.css'
 
@@ -29,7 +30,7 @@ const ActivityForm = () => {
           setComments(response.data[0].comments)
           setDifficultyRating(response.data[0].difficultyRating)
           setDistanceUnit(response.data[0].distanceUnit)
-          setType(response.data[0].type)
+          setType(response.data[0].activityCode)
         }
       } catch (error) {
         console.log(error)
@@ -71,49 +72,79 @@ const ActivityForm = () => {
   return (
 	  formSubmitted ? <Redirect to='/calendar' />
       : <div className={classes.pageContainer}>
+        <div className={classes.pageHeader}>
+          <h1>Manual Entry</h1>
+
+        </div>
         <form onSubmit={submitHandler} className={classes.form}>
-          <div className={classes.inputContainer}>
-            <label>Activity Title</label>
-            <input value={activityTitle} onChange={(e) => setActivityTitle(e.target.value)} type='text' />
+          <div className={classes.formRow1}>
+            <div className={classes.inputContainer}>
+              <label>Activity Title</label>
+              <input value={activityTitle} onChange={(e) => setActivityTitle(e.target.value)} type='text' />
+            </div>
+            <div className={classes.inputContainer}>
+              <label>Type</label>
+              <select onChange={(e) => setType(e.target.value)} value={type}>
+                <option value='1'>Running</option>
+                <option value='2'>Walking</option>
+                <option value='3'>Biking</option>
+                <option value='4'>Swimming</option>
+              </select>
+            </div>
           </div>
-          <div className={classes.inputContainer}>
-            <label>Type</label>
-            <select onChange={(e) => setType(e.target.value)} value={type}>
-              <option value='1'>Running</option>
-              <option value='2'>Walking</option>
-              <option value='3'>Biking</option>
-              <option value='4'>Swimming</option>
-            </select>
+          <div className={classes.formRow2}>
+            <div className={classes.inputContainer}>
+              <label>Distance</label>
+              <input value={distance} onChange={(e) => setDistance(e.target.value)} type='text' />
+            </div>
+            <div className={classes.inputContainer}>
+              <label>Distance Unit</label>
+              <select onChange={(e) => setDistanceUnit(e.target.value)} value={distanceUnit}>
+                <option value='Mi'>Mi</option>
+                <option value='Km'>Km</option>
+              </select>
+            </div>
           </div>
-          <div className={classes.inputContainer}>
-            <label>Distance</label>
-            <input value={distance} onChange={(e) => setDistance(e.target.value)} type='text' />
+          <div className={classes.formRow3}>
+            <div className={classes.inputContainer}>
+              <label>Date</label>
+              <input value={date} onChange={(e) => setDate(e.target.value)} type='text' />
+            </div>
+            <div className={classes.inputContainer}>
+              <label>Elapsed Time</label>
+              <input value={elapsedTime} onChange={(e) => setElapsedTime(e.target.value)} type='text' />
+            </div>
           </div>
-          <div className={classes.inputContainer}>
-            <label>Distance Unit</label>
-            <select onChange={(e) => setDistanceUnit(e.target.value)} value={distanceUnit}>
-              <option value='Mi'>Mi</option>
-              <option value='Km'>Km</option>
-            </select>
-          </div>
-          <div className={classes.inputContainer}>
-            <label>Date</label>
-            <input value={date} onChange={(e) => setDate(e.target.value)} type='text' />
-          </div>
-          <div className={classes.inputContainer}>
-            <label>Elapsed Time</label>
-            <input value={elapsedTime} onChange={(e) => setElapsedTime(e.target.value)} type='text' />
+          <div className={classes.formRow4}>
+            <div className={classes.inputContainer}>
+              <label>Pace per Mi</label>
+              <input></input>
+              <label>Pace per Km</label>
+              <input></input>
+            </div>
+            <div className={classes.inputContainer}>
+              <label>Difficulty Rating</label>
+              <select value={difficultyRating} onChange={(e) => setDifficultyRating(e.target.value)} type='text' >
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
+                <option value='4'>4</option>
+                <option value='5'>5</option>
+                <option value='6'>6</option>
+                <option value='7'>7</option>
+                <option value='8'>8</option>
+                <option value='9'>9</option>
+                <option value='10'>10</option>
+
+              </select>
+            </div>
           </div>
           <div className={classes.inputContainer}>
             <label>Comments</label>
-            <input value={comments} onChange={(e) => setComments(e.target.value)} type='text' />
+            <textarea value={comments} onChange={(e) => setComments(e.target.value)} />
           </div>
           <div className={classes.inputContainer}>
-            <label>Difficulty Rating</label>
-            <input value={difficultyRating} onChange={(e) => setDifficultyRating(e.target.value)} type='text' />
-          </div>
-          <div className={classes.inputContainer}>
-            <button type='submit'>Submit</button>
+            <Button as='button' type='submit'>Submit</Button>
           </div>
         </form>
       </div>
