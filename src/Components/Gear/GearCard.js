f1c279bf2143919307f7e401d8286c79228058e0
Taylor Mills <trmills@eagles.usi.edu>
2021-03-13 09:52:05 -0600

    gear

--- src/Components/Gear/GearCard.js
+++ src/Components/Gear/GearCard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Icon, Progress } from 'semantic-ui-react'
+import { formatDateMMDD } from '../../Lib/time'
 import classes from './gear.module.css'
 
 const GearCard = ({ gear, size }) => {
@@ -12,7 +13,7 @@ const GearCard = ({ gear, size }) => {
       <div className={classes.cardDetailsContainer}>
         <p>{`${gear.brand} ${gear.model}`}</p>
         <p>{gear.colorway}</p>
-        <p>03/05/2000 - Current</p>
+        <p>{formatDateMMDD(gear?.dateAdded)} - Current</p>
       </div>
       <div className={classes.cardProgressContainer}>
         <Progress size='medium' value={gear.miles} total={gear.maxMiles} label={`${gear.miles} / ${gear.maxMiles} Mi`} />
