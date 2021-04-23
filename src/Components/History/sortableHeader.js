46859ed2df0cd640f02eaf03274b65755d7675e6
Taylor Mills <trmills@eagles.usi.edu>
2021-04-22 20:06:58 -0500

    sortable header

---
+++ src/Components/History/sortableHeader.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Icon } from 'semantic-ui-react'
+
+const SortableHeader = ({ children, sorted, name, direction, onClick }) => {
+
+    const clickHandler = (e) => {
+        const newDirection = !sorted ? 'DESC' : direction === 'DESC' ? 'ASC' : 'DESC'
+
+        onClick(e, { direction: newDirection, column: name })
+    }
+
+    return (
+        <th onClick={clickHandler} style={{ cursor: 'pointer' }}>
+            {children}
+            {sorted && <Icon style={{ color: 'white', marginLeft: 3 }} size='tiny' name={direction === 'DESC' ? 'angle down' : 'angle up'} />}
+        </th>
+    )
+
+
+}
+
+export default SortableHeader
