a8e4cb7c1c7ce3e455da13b609969173200ffc81
Taylor Mills <trmills@eagles.usi.edu>
2021-04-22 20:07:18 -0500

    Pagination, sorting, search

--- src/Components/History/History.js
+++ src/Components/History/History.js
@@ -1,143 +1,81 @@
 import React, { useEffect, useState } from 'react'
-import { Button } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
+import { Pagination, Search } from 'semantic-ui-react'
 import { calculatePacePerMile } from '../../Lib/pace'
 import { getActivities } from '../../Services/activities'
+import useDebounce from '../../Hooks/useDebounce'
+import SortableHeader from './sortableHeader'
 import classes from './history.module.css'
 
 const History = () => {
-  /* const runs = [
-    {
-      date: '11-04-20',
-      type: 'Run',
-      activityTitle: 'Easy Run',
-      distance: 8,
-      activityId: 1
-    },
-    {
-      date: '11-08-20',
-      type: 'Run',
-      activityTitle: 'Long Run',
-      distance: 15,
-      activityId: 2
-    },
-    {
-      date: '11-09-20',
-      type: 'Run',
-      activityTitle: 'Long Fartlek',
-      distance: 9,
-      activityId: 3
-    },
-    {
-      date: '11-10-20',
-      type: 'Run',
-      activityTitle: 'Easy Run',
-      distance: 7,
-      activityId: 4
-    },
-    {
-      date: '11-11-20',
-      type: 'Run',
-      activityTitle: 'Tempo',
-      distance: 10,
-      activityId: 5
-    },
-    {
-      date: '11-12-20',
-      type: 'Run',
-      activityTitle: 'Easy Run',
-      distance: 7,
-      activityId: 6
-    },
-    {
-      date: '11-13-20',
-      type: 'Run',
-      activityTitle: 'Medium Long Run',
-      distance: 14,
-      activityId: 7
-    },
-    {
-      date: '11-14-20',
-      type: 'Run',
-      activityTitle: 'Recovery Run',
-      distance: 6,
-      activityId: 8
-    },
-    {
-      date: '11-15-20',
-      type: 'Run',
-      activityTitle: 'Recovery Run',
-      distance: 10,
-      activityId: 9
-    },
-    {
-      date: '11-16-20',
-      type: 'Run',
-      activityTitle: 'Hill Repeats',
-      distance: 4.5,
-      activityId: 10
-    },
-    {
-      date: '11-17-20',
-      type: 'Run',
-      activityTitle: 'Easy Run',
-      distance: 7,
-      activityId: 11
-    },
-    {
-      date: '11-18-20',
-      type: 'Run',
-      activityTitle: 'Warmup',
-      distance: 3,
-      activityId: 12
-    },
-    {
-      date: '11-19-20',
-      type: 'Run',
-      activityTitle: 'Cooldown',
-      distance: 3,
-      activityId: 13
-    }
-  ] */
-
+  const LIMIT = 10
   const [activities, setActivities] = useState([])
+  const [offset, setOffset] = useState(0)
+  const [pages, setPages] = useState(0)
+  const [count, setCount] = useState(0)
+  const [activePage, setActivePage] = useState(1)
+  const [searchTerm, setSearchTerm] = useState('')
+  const [column, setColumn] = useState('date')
+  const [sortDirection, setSortDirection] = useState('DESC')
+  const [debouncedSearchTerm] = useDebounce(searchTerm, 500)
 
   useEffect(() => {
     const loadActivities = async () => {
       try {
-        const results = await getActivities()
+        const results = await getActivities({ limit: LIMIT, offset, searchTerm: debouncedSearchTerm, column, sortDirection })
 
-        setActivities(results?.data)
+        setActivities(results?.data?.activities)
+        setPages(results?.data?.pages)
+        setCount(results?.data?.count)
       } catch (error) {
         console.log('Error getting history', error)
       }
     }
 
     loadActivities()
-  }, [])
+  }, [activePage, debouncedSearchTerm, sortDirection, column])
+
+  const pageChangeHandler = (e, data) => {
+    setOffset((data.activePage - 1) * LIMIT)
+    setActivePage(data.activePage)
+  }
+
+  const searchChangeHandler = (e, data) => {
+    setSearchTerm(data.value)
+  }
+
+  const sortDirectionHandler = (e, data) => {
+    setSortDirection(data?.direction)
+    setColumn(data?.column)
+  }
+
   return (
-        <div className={classes.historyPageContainer}>
-          <table className={classes.historyTable}>
-            <tr className={classes.historyTableHeader}>
-              <th>Date</th>
-              <th>Type</th>
-              <th>Title</th>
-              <th style={{ textAlign: 'right' }}>Distance</th>
-              <th style={{ textAlign: 'right' }}>Pace</th>
-              <th>Action</th>
-            </tr>
-                {activities.map((activity) => (
-                  <tr key={activity.activityId} className={classes.tableBodyRow}>
-                  <td>{activity.date}</td>
-                  <td>{activity.type}</td>
-                  <td>{activity.title}</td>
-                  <td style={{ textAlign: 'right' }}>{`${activity.distance} Mi`}</td>
-                  <td style={{ textAlign: 'right' }}>{calculatePacePerMile(activity.elapsedTime, activity.distance, 'Mi')}</td>
-                  <td><a href={`/activityView/${activity.id}`}>View</a></td>
-                  </tr>
-                ))}
-          </table>
-        </div>
+    <div className={classes.historyPageContainer}>
+      <div style={{ width: '50%' }}>
+        <Search style={{ marginTop: 20 }} value={searchTerm} onSearchChange={searchChangeHandler} showNoResults={false} />
+      </div>
+      <table className={classes.historyTable}>
+        <tr className={classes.historyTableHeader}>
+          <SortableHeader onClick={sortDirectionHandler} name='date' sorted={column === 'date'} direction={sortDirection}>Date</SortableHeader>
+          <th>Type</th>
+          <SortableHeader onClick={sortDirectionHandler} name='title' sorted={column === 'title'} direction={sortDirection}>Title</SortableHeader>
+          <SortableHeader onClick={sortDirectionHandler} name='distance' sorted={column === 'distance'} direction={sortDirection} style={{ textAlign: 'right' }}>Distance</SortableHeader>
+          <th style={{ textAlign: 'right' }}>Pace</th>
+          <th>Action</th>
+        </tr>
+        {activities?.map((activity) => (
+          <tr key={activity.activityId} className={classes.tableBodyRow}>
+            <td>{activity.date}</td>
+            <td>{activity.type}</td>
+            <td>{activity.title}</td>
+            <td style={{ textAlign: 'right' }}>{`${activity.distance} Mi`}</td>
+            <td style={{ textAlign: 'right' }}>{calculatePacePerMile(activity.elapsedTime, activity.distance, 'Mi')}</td>
+            <td><a href={`/activityView/${activity.id}`}>View</a></td>
+          </tr>
+        ))}
+      </table>
+      <Pagination style={{ marginTop: 25 }} onPageChange={pageChangeHandler} totalPages={pages} activePage={activePage} />
+      <p style={{ marginTop: 10 }}>{`Viewing ${activities?.length + offset} of ${count} Records`}</p>
+    </div >
   )
 }
 
