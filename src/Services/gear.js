df1992ffe4f459ef8d97f66fc446942b39097d75
Taylor Mills <taylormills190@gmail.com>
2021-09-19 16:08:32 -0500

    more linting

--- src/Services/gear.js
+++ src/Services/gear.js
@@ -1,25 +1,25 @@
 import axios from 'axios'
 
 export const getGear = async (query) => {
-  return await axios.get('http://localhost:5000/gear', { params: query, withCredentials: true })
+	return await axios.get('http://localhost:5000/gear', { params: query, withCredentials: true })
 }
 
 export const getGearById = async (gearId) => {
-  return await axios.get(`http://localhost:5000/gear/${gearId}`, { withCredentials: true })
+	return await axios.get(`http://localhost:5000/gear/${gearId}`, { withCredentials: true })
 }
 
 export const patchGear = async (gearId, changes) => {
-  return await axios.patch(`http://localhost:5000/gear/${gearId}`, { changes }, { withCredentials: true })
+	return await axios.patch(`http://localhost:5000/gear/${gearId}`, { changes }, { withCredentials: true })
 }
 
 export const updateGear = async (gear) => {
-  return await axios.put('http://localhost:5000/gear', { gear }, { withCredentials: true })
+	return await axios.put('http://localhost:5000/gear', { gear }, { withCredentials: true })
 }
 
 export const postGear = async (gear) => {
-  return await axios.post('http://localhost:5000/gear', { gear }, { withCredentials: true })
+	return await axios.post('http://localhost:5000/gear', { gear }, { withCredentials: true })
 }
 
 export const deleteGear = async (gearId) => {
-  return await axios.delete(`http://localhost:5000/gear/${gearId}`, { withCredentials: true })
+	return await axios.delete(`http://localhost:5000/gear/${gearId}`, { withCredentials: true })
 }
